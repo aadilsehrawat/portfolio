0c4314fa6e45f7cd003e6d15a043561f5ea7eb5a
aadilsehrawat <aadilsehrawat@gmail.com>
2024-03-21 19:30:51 +0530

    Added views and urls for about, contact, projects and services templates

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', index, name='index'),
+    path('home/', home, name='home'),
+    path('about/', about, name='about'),
+    path('projects/', projects, name='projects'),
+    path('services/', services, name='services'),
+    path('contact/', contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return redirect('home')
 def home(request):
     return render(request, 'main/home.html')
+def about(request):
+    return render(request, 'main/about.html')
+def projects(request):
+    return render(request, 'main/projects.html')
+def services(request):
+    return render(request, 'main/services.html')
+def contact(request):
+    return render(request, 'main/contact.html')
