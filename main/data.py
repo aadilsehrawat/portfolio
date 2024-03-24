4e616dbd800ec725a5981ab9fdd30debaf282ab9
aadilsehrawat <aadilsehrawat@gmail.com>
2024-03-24 09:09:40 +0530

    Dummy data for projects, services and socials page

---
+++ main/data.py
@@ -0,0 +1,92 @@
+projects = [
+    {
+        'id': 1,
+        'name': 'Portfolio Web-App',
+        'url': 'https://www.aadilsehrawat.me/',
+        'project_image_url': 'images/socials/black-on-white/linkedin.png'
+    },
+    {
+        'id': 2,
+        'name': 'Jelly Bot',
+        'url': 'https://jelly-bot.vercel.app/',
+        'project_image_url': 'images/socials/black-on-white/linkedin.png'
+    },
+    {
+        'id': 3,
+        'name': 'ReQR - QR Code Regenerator',
+        'url': 'https://www.github.com/aadilsehrawat/',
+        'project_image_url': 'images/socials/black-on-white/github.png'
+    },
+]
+
+services = [
+    {
+        'id': 1,
+        'name': 'Resume Review',
+        'url': 'https://www.linkedin.com/in/aadilsehrawat/',
+        'service_image_url': 'images/socials/black-on-white/linkedin.png'
+    },
+    {
+        'id': 2,
+        'name': 'Interview Preparation',
+        'url': 'https://www.github.com/aadilsehrawat/',
+        'service_image_url': 'images/socials/black-on-white/github.png'
+    },
+    {
+        'id': 3,
+        'name': '1 on 1 Mentorship',
+        'url': 'https://www.leetcode.com/aadilsehrawat/',
+        'service_image_url': 'images/socials/black-on-white/leetcode.png'
+    },
+    {
+        'id': 4,
+        'name': 'Career Guidance',
+        'url': 'https://www.instagram.com/aadilsehrawat/',
+        'service_image_url': 'images/socials/black-on-white/instagram.png'
+    },
+    {
+        'id': 5,
+        'name': 'Technical Content Writing',
+        'url': 'https://www.facebook.com/aadilsehrawat/',
+        'service_image_url': 'images/socials/black-on-white/facebook.png'
+    },
+]
+
+socials = [
+    {
+        'id': 1,
+        'name': 'LinkedIn',
+        'url': 'https://www.linkedin.com/in/aadilsehrawat/',
+        'social_image_url': 'images/socials/black-on-white/linkedin.png'
+    },
+    {
+        'id': 2,
+        'name': 'GitHub',
+        'url': 'https://www.github.com/aadilsehrawat/',
+        'social_image_url': 'images/socials/black-on-white/github.png'
+    },
+    {
+        'id': 3,
+        'name': 'LeetCode',
+        'url': 'https://www.leetcode.com/aadilsehrawat/',
+        'social_image_url': 'images/socials/black-on-white/leetcode.png'
+    },
+    {
+        'id': 4,
+        'name': 'Instagram',
+        'url': 'https://www.instagram.com/aadilsehrawat/',
+        'social_image_url': 'images/socials/black-on-white/instagram.png'
+    },
+    {
+        'id': 5,
+        'name': 'Facebook',
+        'url': 'https://www.facebook.com/aadilsehrawat/',
+        'social_image_url': 'images/socials/black-on-white/facebook.png'
+    },
+    {
+        'id': 6,
+        'name': 'Twitter',
+        'url': 'https://www.twitter.com/aadilsehrawat/',
+        'social_image_url': 'images/socials/black-on-white/twitter.png'
+    },
+]
