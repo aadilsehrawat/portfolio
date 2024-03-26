fe0baa21d525a2bd6f98e6ae949c1443a7d2802c
aadilsehrawat <aadilsehrawat@gmail.com>
2024-03-26 16:53:29 +0530

    Updated skills data

--- main/data.py
+++ main/data.py
@@ -1,3 +1,108 @@
+skills=[
+    {'id':1,
+    'category':'Programming Languages',
+    'name':'Python',
+    'url':'https://www.python.org/',
+    'skill_image_url':'images/skills-and-projects/skills/python.png'
+    },
+    {'id':2,
+    'category':'Programming Languages',
+    'name':'C',
+    'url':'https://en.wikipedia.org/wiki/C_(programming_language)',
+    'skill_image_url':'images/skills-and-projects/skills/c.png'
+    },
+    {'id':3,
+    'category':'Programming Languages',
+    'name':'C++',
+    'url':'https://en.wikipedia.org/wiki/C%2B%2B',
+    'skill_image_url':'images/skills-and-projects/skills/cpp.png'
+    },
+    {'id':4,
+    'category':'Frontend',
+    'name':'HTML',
+    'url':'https://developer.mozilla.org/en-US/docs/Web/HTML',
+    'skill_image_url':'images/skills-and-projects/skills/html.png'
+    },
+    {'id':5,
+    'category':'Frontend',
+    'name':'CSS',
+    'url':'https://developer.mozilla.org/en-US/docs/Web/CSS',
+    'skill_image_url':'images/skills-and-projects/skills/css.png'
+    },
+    {'id':6,
+    'category':'Frontend',
+    'name':'JavaScript',
+    'url':'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    'skill_image_url':'images/skills-and-projects/skills/javascript.png'
+    },
+    {'id':7,
+    'category':'Frontend',
+    'name':'Vue JS',
+    'url':'https://vuejs.org/',
+    'skill_image_url':'images/skills-and-projects/skills/vue.png'
+    },
+    {'id':8,
+    'category':'Frontend',
+    'name':'Tailwind CSS',
+    'url':'https://tailwindcss.com/',
+    'skill_image_url':'images/skills-and-projects/skills/tailwind.png'
+    },
+    {'id':9,
+    'category':'Frontend',
+    'name':'Tailblocks',
+    'url':'https://tailblocks.cc/',
+    'skill_image_url':'images/skills-and-projects/skills/tailblocks.png'
+    },
+    {'id':10,
+    'category':'Backend',
+    'name':'Django',
+    'url':'https://www.djangoproject.com/',
+    'skill_image_url':'images/skills-and-projects/skills/django.png'
+    },
+    {'id':11,
+    'category':'Backend',
+    'name':'Flask',
+    'url':'https://flask.palletsprojects.com/',
+    'skill_image_url':'images/skills-and-projects/skills/flask.png'
+    },
+    {'id':12,
+    'category':'Backend',
+    'name':'Celery',
+    'url':'https://docs.celeryq.dev/en/stable/',
+    'skill_image_url':'images/skills-and-projects/skills/celery.png'
+    },
+    {'id':13,
+    'category':'Databases',
+    'name':'PostgreSQL',
+    'url':'https://www.postgresql.org/',
+    'skill_image_url':'images/skills-and-projects/skills/postgresql.png'
+    },
+    {'id':14,
+    'category':'Databases',
+    'name':'MongoDB',
+    'url':'https://www.mongodb.com/',
+    'skill_image_url':'images/skills-and-projects/skills/mongodb.png'
+    },
+    {'id':15,
+    'category':'Version Control',
+    'name':'Git',
+    'url':'https://git-scm.com/',
+    'skill_image_url':'images/skills-and-projects/skills/git.png'
+    },
+    {'id':16,
+    'category':'Version Control',
+    'name':'GitHub',
+    'url':'https://www.github.com/',
+    'skill_image_url':'images/skills-and-projects/skills/github.png'
+    },
+    {'id':17,
+    'category':'Deployment',
+    'name':'Vercel',
+    'url':'https://vercel.com/',
+    'skill_image_url':'images/skills-and-projects/skills/vercel.png'
+    },
+]
+
 projects = [
     {
         'id': 1,
