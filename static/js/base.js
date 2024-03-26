52ecbbcad2cf40d073f2c8330117cd1e677bff12
aadilsehrawat <aadilsehrawat@gmail.com>
2024-03-26 23:45:21 +0530

    Content top margin made static instead of dynamic

--- static/js/base.js
+++ static/js/base.js
@@ -2,6 +2,5 @@ window.onload = function() {
     var headerHeight = document.getElementById('header').offsetHeight;
     var footerHeight = document.getElementById('footer').offsetHeight;
     var contentHeight = window.innerHeight - (headerHeight + footerHeight);
-    document.querySelector('#content').style.marginTop = headerHeight + 'px';
     document.querySelector('#content').style.minHeight = contentHeight + 'px';
 };
